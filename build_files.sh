c27151d04124d24a4d455fec67d0fcc622e8c646
Caio Madeira <caiomadeiracontato@outlook.com>
2024-06-08 18:52:21 -0300

    adiciontal configuration in version.json

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,18 @@
 echo "BUILD START" 
 
-python3.11.6 --version
-python3.11.6 -m ensurepip 
-python3.11.6 -m pip install -r requirements.txt 
-python3.11.6 manage.py collectstatic --noinput --clear 
+python3.9 --version
+python3.9 -m ensurepip 
+python3.9 -m pip install -r requirements.txt 
+python3.9 manage.py collectstatic
 
 echo "INSTALL PLAYWRIGHT"
-python3.11.6 -m playwright install
+python3.9 -m playwright install
+echo "playwright installed sucess"
+
+echo " MAKE MIGRATIONS..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
--- sau_auth_service/settings.py
+++ sau_auth_service/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'sau_auth_service.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
