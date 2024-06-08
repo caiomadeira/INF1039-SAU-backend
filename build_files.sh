a1a29d98c9e8717c438fbc108702fa4af2d7f545
Caio Madeira <caiomadeiracontato@outlook.com>
2024-06-08 19:00:37 -0300

    Dummy commit to trigger something

--- build_files.sh
+++ build_files.sh
@@ -9,9 +9,9 @@ echo "INSTALL PLAYWRIGHT"
 python3.9 -m playwright install
 echo "playwright installed sucess"
 
-echo " MAKE MIGRATIONS..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+#echo " MAKE MIGRATIONS..."
+#python3.9 manage.py makemigrations --noinput
+#python3.9 manage.py migrate --noinput
 
 python3.9 manage.py collectstatic --noinput --clear
 
