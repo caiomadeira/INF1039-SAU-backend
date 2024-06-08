8aa550cb8f21ced755553a224defa072db31ad59
Caio Madeira <caiomadeiracontato@outlook.com>
2024-06-08 19:18:00 -0300

    playwright decrease version

--- build_files.sh
+++ build_files.sh
@@ -5,15 +5,16 @@ python3.9 -m ensurepip
 python3.9 -m pip install -r requirements.txt 
 python3.9 manage.py collectstatic
 
-echo "GLIBC VERSION"
+echo ">>>>>>>GLIBC VERSION"
 ldd --version
+echo "================================"
 
-echo "INSTALL PLAYWRIGHT"
+echo ">>>>>>INSTALL PLAYWRIGHT"
 python3.9 -m playwright install
 python3.9 -m playwright install-deps
 apt-get install libnss3\libnspr4\libgbm1                                      
 
-echo "playwright installed sucess"
+echo "==================== playwright install END ================================"
 
 #echo " MAKE MIGRATIONS..."
 #python3.9 manage.py makemigrations --noinput
