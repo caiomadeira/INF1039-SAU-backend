cee9b33a703efdc7e470c2886df6505585521f12
Caio Madeira <caiomadeiracontato@outlook.com>
2024-06-08 19:10:43 -0300

    update build files

--- build_files.sh
+++ build_files.sh
@@ -6,10 +6,13 @@ python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
 
 echo "GLIBC VERSION"
-$ ldd --version
+ldd --version
 
 echo "INSTALL PLAYWRIGHT"
 python3.9 -m playwright install
+python3.9 -m playwright install-deps
+apt-get install libnss3\libnspr4\libgbm1                                      
+
 echo "playwright installed sucess"
 
 #echo " MAKE MIGRATIONS..."
